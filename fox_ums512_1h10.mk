40440c922ce1a5bf05ab78d609c81f4875c68c8b
J <40410860+Seriousattempts@users.noreply.github.com>
2025-05-22 00:55:31 -0400

    Update fox_ums512_1h10.mk

--- fox_ums512_1h10.mk
+++ fox_ums512_1h10.mk
@@ -1,10 +1,14 @@
-# OrangeFox settings for UMS512_1H10
-OF_HIDE_NOTCH := 0  
+# OrangeFox Recovery Configuration  
+PRODUCT_COPY_FILES += \  
+    device/sprd/ums512_1h10/recovery/root/system/bin/ums512_utils:$(TARGET_COPY_OUT_SYSTEM)/bin/ums512_utils  
+
+# Display Parameters  
+OF_SCREEN_H := 1600  
+OF_STATUS_H := 80  
+OF_STATUS_INDENT_LEFT := 48  
+OF_STATUS_INDENT_RIGHT := 48  
+
+# Hardware Flags  
 OF_USE_GREEN_LED := 0  
 OF_FLASHLIGHT_ENABLE := 1  
 OF_ALLOW_DISABLE_NAVBAR := 0  
-OF_SPLASH_MAX_SIZE := "65"  
-OF_ADVANCED_SECURITY := 0  
-OF_SCREEN_H := 1600  # From device specs[1][34]  
-OF_STATUS_H := 80  
-OF_CLOCK_POS := 1  
