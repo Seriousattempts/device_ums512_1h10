bf7966249b9decd2789234263f818c4495d87a4f
J <40410860+Seriousattempts@users.noreply.github.com>
2025-05-22 00:22:13 -0400

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash  
+export FOX_VERSION="R12.1"  
+export FOX_VARIANT="A12"  
+export FOX_BUILD_DEVICE="ums512_1h10"  
+export OF_MAINTAINER="YourName"  
+export OF_DYNAMIC_FULL_SIZE=9126805504
+
+lunch twrp_ums512_1h10-eng  
